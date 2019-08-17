08002654599e4b9a33495246fffd498b668b4f47
Vasiliy Gnativ <gnativ.vasiliy@gmail.com>
2019-08-18 02:00:56 +0300

    form7

--- assets/php/data-contact.php
+++ assets/php/data-contact.php
@@ -33,15 +33,5 @@
   } catch (Exception $e) {
 
   }
-  server {
-    listen 80;
-    server_name localhost;
-    location / {
-    root html;
-    index index.html index.htm;
-    }
-    error_page 405 =200 $uri;
-    # ...
-    }
 
 ?>
